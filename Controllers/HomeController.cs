74e58e880f28a060acbe7fa3fe87774f6e131cf5
Rafael Tongson <rtongson@peppymiint.local>
2019-10-24 14:09:05 -0700

    dependency injection within controller for context

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using CRUDelicious.Models;
+using Microsoft.EntityFrameworkCore;
+
+using System.Linq;
+using System.Collections.Generic;
 
 namespace CRUDelicious.Controllers
 {
     public class HomeController : Controller
     {
+        // Dependency Injection
+        private CruddyContext dbContext;
+        // here we can "inject" our context service into the constructor
+        public HomeController(CruddyContext context)
+        {
+            dbContext = context;
+        }
+
         [HttpGet("")]
         public IActionResult Index()
         {
+            List<Dish> AllDishes = dbContext.Dishes.ToList();
             return View();
         }
     
