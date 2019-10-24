f2eb0b6064562b22576b5a3516c2dc7e3f6c8e4f
Rafael Tongson <rtongson@peppymiint.local>
2019-10-24 14:32:59 -0700

    refactor startup

--- Startup.cs
+++ Startup.cs
@@ -32,12 +32,16 @@ namespace CRUDelicious
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //////////////////////////////
+            //   OLD DB CONNECTION    //
             //With our Context class created we need to add it as a service so that we can use dependency injection with our controllers.  
             // string mySqlConnection = "server=localhost;userid=<<<user>>>;password=<<<pw>>>;port=<<<portnum>>>;database=<<<<dbname>>>>;SslMode=None";
 
             //inject the context we made
             // services.AddDbContext<CruddyContext>(options => options.UseMySql(mySqlConnection));
 
+            // END - OLD DB CONNECTION//
+            ///////////////////////////
 
             // Now we may use the connection string itself, bound to Configuration, to pass the required connection
             // credentials to MySQL
