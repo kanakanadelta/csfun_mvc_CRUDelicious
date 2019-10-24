0eec045d2ecc1b88b0078066fa3e138541e6d22d
Rafael Tongson <rtongson@peppymiint.local>
2019-10-24 14:16:30 -0700

    refactor startup with securing connection

--- Startup.cs
+++ Startup.cs
@@ -10,21 +10,40 @@ using Microsoft.Extensions.DependencyInjection;
 //Register Context Class as a service
 using CRUDelicious.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace CRUDelicious
 {
     public class Startup
     {
+        // This public getter will be how you access the data from appsettings.json
+        // To access the connection string itself, you would use the following:
+        // Configuration["DBInfo:ConnectionString"]
+        public IConfiguration Configuration {get;}
+
+        // Here we will "inject" the default IConfiguration service, which will bind to appsettings.json by default
+        // and then assign it to the Configuration property.  This happens at the startup of our application.
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             //With our Context class created we need to add it as a service so that we can use dependency injection with our controllers.  
-            string mySqlConnection = "server=localhost;userid=root;password=root;port=3306;database=crudelicious;SslMode=None";
+            // string mySqlConnection = "server=localhost;userid=<<<user>>>;password=<<<pw>>>;port=<<<portnum>>>;database=<<<<dbname>>>>;SslMode=None";
 
             //inject the context we made
-            services.AddDbContext<CruddyContext>(options => options.UseMySql(mySqlConnection));
+            // services.AddDbContext<CruddyContext>(options => options.UseMySql(mySqlConnection));
 
+
+            // Now we may use the connection string itself, bound to Configuration, to pass the required connection
+            // credentials to MySQL
+            services.AddDbContext<CruddyContext>(options => options.UseMySql(Configuration["DBInfo:ConnectionString"]));
+
+            //other services
             services.AddMvc();
         }
 
